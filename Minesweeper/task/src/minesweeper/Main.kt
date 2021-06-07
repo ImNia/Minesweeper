69c57c1cf7d2ff0782a9d147d05bbbcb42086102
ImNia <agnia-kimi1996@mail.ru>
2021-06-07 09:55:42 +0700

    3 stage

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -2,22 +2,57 @@ package minesweeper
 
 import kotlin.random.Random
 
+fun addAround(field: Array<CharArray>, sizeField: Int): Array<CharArray> {
+    for (x in 0 until sizeField) {
+        for (y in 0 until sizeField) {
+            if (field[x][y] != 'X') {
+
+                for (i in -1..1) {
+                    for (j in -1..1) {
+                        if (x + i in 0 until sizeField && y + j in 0 until sizeField && field[x + i][y + j] == 'X') {
+                            when (field[x][y]) {
+                                '.' -> field[x][y] = '1'
+                                '1' -> field[x][y] = '2'
+                                '2' -> field[x][y] = '3'
+                                '3' -> field[x][y] = '4'
+                                '4' -> field[x][y] = '5'
+                                '5' -> field[x][y] = '6'
+                                '6' -> field[x][y] = '7'
+                                '7' -> field[x][y] = '8'
+                                '8' -> field[x][y] = '9'
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    return field
+}
+
+fun addMine(field: Array<CharArray>): Array<CharArray> {
+    var x: Int
+    var y: Int
+    do {
+        x = Random.nextInt(0, 9)
+        y = Random.nextInt(0, 9)
+    } while (field[x][y] != '.')
+    field[x][y] = 'X'
+    return field
+}
+
 fun main() {
     val sizeField = 9
-    val field: Array<CharArray> = Array(sizeField) { CharArray(sizeField) {'.'} }
+    var field: Array<CharArray> = Array(sizeField) { CharArray(sizeField) {'.'} }
 
     print("How many mines do you want on the field? > ")
     val countMine = readLine()!!.toInt()
 
     for (i in 0 until countMine) {
-        var x: Int
-        var y: Int
-        do {
-            x = Random.nextInt(0, 9)
-            y = Random.nextInt(0, 9)
-        } while (field[x][y] != '.')
-        field[x][y] = 'X'
+        field = addMine(field)
     }
+    field = addAround(field, sizeField)
 
     for (i in 0 until sizeField) {
         println(field[i].joinToString(separator=""))
